66dbfcea35ae21bea8ad59e74edb6d393cd2323c
Escobar <lukeescobar73@gmail.com>
2025-11-01 14:27:54 -0300

    Implementa funcao maior palavra

--- solucoes.py
+++ solucoes.py
@@ -1,8 +1,20 @@
+import string
+
 def sao_anagramas(string1, string2):
     pass
 
 def cifra_de_cesar(texto, deslocamento):
     pass
 
-def valida_cpf(cpf_string):
-    pass
+def encontrar_maior_palavra(frase):
+    palavras = frase.split()
+    maior_palavra = ""
+    maior_tamanho = 0
+
+    for palavra in palavras:
+        limpa = palavra.strip(string.punctuation)
+        if len(limpa) > maior_tamanho:
+            maior_tamanho = len(limpa)
+            maior_palavra = limpa
+
+    return maior_palavra

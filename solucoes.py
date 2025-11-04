14b24980f25d9a687b95ea26b80e8d7696e707cf
FernandoGLima <fernandolimabusiness@gmail.com>
2025-11-04 10:13:46 -0300

    feat: implementa funcao de verificacao de anagramas

--- solucoes.py
+++ solucoes.py
@@ -1,5 +1,16 @@
 def sao_anagramas(string1, string2):
-    pass
+    vector1=[0]*26
+    vector2=[0]*26
+
+    for ch in string1.lower():
+        if 'a' <= ch <= 'z':
+            vector1[ord(ch)-97] += 1
+
+    for ch in string2.lower():
+        if 'a' <= ch <= 'z':
+            vector2[ord(ch)-97] += 1
+
+    return vector1 == vector2
 
 def cifra_de_cesar(texto, deslocamento):
     pass

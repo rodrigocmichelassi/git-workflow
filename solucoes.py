21a351842543e1a28152e2f44fa4f3ffc1202ccc
FernandoGLima <fernandolimabusiness@gmail.com>
2025-11-05 10:50:34 -0300

    refactor: renomeia variável de 'ch' para 'character' para melhor clareza

--- solucoes.py
+++ solucoes.py
@@ -2,13 +2,13 @@ def sao_anagramas(string1, string2):
     vector1=[0]*26
     vector2=[0]*26
 
-    for ch in string1.lower():
-        if 'a' <= ch <= 'z':
-            vector1[ord(ch)-97] += 1
+    for character in string1.lower():
+        if 'a' <= character <= 'z':
+            vector1[ord(character)-97] += 1
 
-    for ch in string2.lower():
-        if 'a' <= ch <= 'z':
-            vector2[ord(ch)-97] += 1
+    for character in string2.lower():
+        if 'a' <= character <= 'z':
+            vector2[ord(character)-97] += 1
 
     return vector1 == vector2
 

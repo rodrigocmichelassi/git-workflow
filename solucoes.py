4e3ffaeb2026fcbbc98bc6b32826b2548e4d0458
rodrigocmichelassi <rodmichelassi@gmail.com>
2025-11-05 21:07:48 -0300

    move sokucao de cifra_de_cesar para solucoes.py

--- solucoes.py
+++ solucoes.py
@@ -2,7 +2,17 @@ def sao_anagramas(string1, string2):
     pass
 
 def cifra_de_cesar(texto, deslocamento):
-    pass
+    texto_codificado = ""
+
+    for letra in texto:
+        if letra.isupper():
+            texto_codificado += chr((ord(letra) - 65 + deslocamento) % 26 + 65)
+        elif letra.islower():
+            texto_codificado += chr((ord(letra) - 97 + deslocamento) % 26 + 97)
+        else:
+            texto_codificado += letra
+
+    return texto_codificado
 
 def valida_cpf(cpf_string):
     pass
